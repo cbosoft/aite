185a5bd026a7dd6e0879272fe9fa0bd154c4f181
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-14 10:10:56 +0100

    Sketched out planet class

---
+++ src/planet/planet.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <list>
+#include <utility>
+#include <memory>
+
+class Element;
+class Planet {
+
+  private:
+
+    double radius;
+    double density_core, density_mantle, density_deep, density_surface;
+    double pressure_low, pressure_high, pressure_average;
+    double temp_low, temp_high, temp_average;
+    double humidity_low, humidity_high, humidity_average;
+    double water_coverage;
+
+    std::list<std::pair<std::shared_ptr<Element>, double>> elemental_makeup;
+
+  public:
+
+    Planet();
+    ~Planet();
+
+    static std::shared_ptr<Planet> generate();
+
+    std::string describe() const;
+};
