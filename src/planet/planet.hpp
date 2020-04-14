6c15bc4d8f66f291a37f15d87780b50371d424ed
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-14 22:20:46 +0100

    Commented out humidity and water coverage planet stats

--- src/planet/planet.hpp
+++ src/planet/planet.hpp
@@ -12,10 +12,10 @@ class Planet {
     double density_core, density_mantle, density_deep, density_surface, density_average;
     double pressure_low, pressure_high, pressure_average;
     double temp_low, temp_high, temp_average;
-    double humidity_low, humidity_high, humidity_average;
-    double water_coverage;
 
     std::list<std::pair<std::shared_ptr<Element>, double>> elemental_makeup;
+    // double humidity_low, humidity_high, humidity_average;
+    // double water_coverage;
 
   public:
 
