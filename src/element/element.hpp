2f13d6094f9c78e0c19834c2186742695079b5e5
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-15 11:41:43 +0100

    Add sort functors for element abundance lists

--- src/element/element.hpp
+++ src/element/element.hpp
@@ -21,8 +21,32 @@ class Element {
     bool is_reactive() const;
     ElementPhase get_phase(double temperature, double pressure) const;
 
+    std::string get_name() const;
+
     static std::list<std::pair<std::shared_ptr<Element>, double>> get_distribution(double density);
     static std::shared_ptr<Element> get_element_by_n(int n);
 
 };
 
+
+class CompositionSortByAbundance {
+
+  public:
+    bool operator()(std::pair<std::shared_ptr<Element>, double> left, std::pair<std::shared_ptr<Element>, double> right)
+    {
+      double aleft = left.second;
+      double aright = right.second;
+      return aleft < aright;
+    }
+};
+
+class CompositionSortByAbundanceDescending {
+
+  public:
+    bool operator()(std::pair<std::shared_ptr<Element>, double> left, std::pair<std::shared_ptr<Element>, double> right)
+    {
+      double aleft = left.second;
+      double aright = right.second;
+      return aleft > aright;
+    }
+};
