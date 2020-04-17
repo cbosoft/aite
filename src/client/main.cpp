0a34d5aca6e1aca7cb48e3687e49fc739b7077b4
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-17 20:42:06 +0100

    Client joins the server by broadcasting its colony name

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,4 @@
+#include "../util/formatter.hpp"
 #include "args.hpp"
 #include "connect.hpp"
 
@@ -6,5 +7,5 @@ int main(int argc, const char **argv)
   ClientArgs args = parse_args(argc, argv);
 
   ServerConnection conn = ServerConnection(args.server_address, args.server_port);
-  conn.send("hello");
+  conn.send(Formatter() << "join|" << args.colony_name);
 }
