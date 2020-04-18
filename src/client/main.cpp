6d9fac0bbb70e884cdab66d06739ba5fd1f1d4e9
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-18 16:59:15 +0100

    Ask server for colony size

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "../util/formatter.hpp"
 #include "args.hpp"
 #include "connect.hpp"
@@ -7,5 +9,10 @@ int main(int argc, const char **argv)
   ClientArgs args = parse_args(argc, argv);
 
   ServerConnection conn = ServerConnection(args.server_address, args.server_port);
-  conn.send(Formatter() << "join|" << args.colony_name);
+
+  std::string reply = conn.send(Formatter() << "join|" << args.colony_name);
+  std::cerr << reply << std::endl;
+
+  reply = conn.send(Formatter() << "query|number");
+  std::cerr << reply << std::endl;
 }
