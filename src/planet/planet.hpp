a2db1c89318ee2e6d27635bce96505752ef3cdc7
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-19 20:44:41 +0100

    Moved planet description types out to own header to tidy up

--- src/planet/planet.hpp
+++ src/planet/planet.hpp
@@ -3,69 +3,7 @@
 #include <utility>
 
 #include "../types.hpp"
-
-enum TemperatureLevel {
-  TL_NearAbsoluteZero,
-  TL_ExtremeCold,
-  TL_VeryCold,
-  TL_Cold,
-  TL_Temperate,
-  TL_Hot,
-  TL_VeryHot,
-  TL_ExtremeHeat,
-  TL_Scorching
-};
-
-enum AtmosphereLevel {
-  AL_None,
-  AL_Thin,
-  AL_EarthLike,
-  AL_Thick,
-  AL_Heavy
-};
-
-enum WindLevel {
-  WL_None,
-  WL_Negligable,
-  WL_Strong,
-  WL_Gale,
-  WL_Ferocious
-};
-
-enum GravityLevel {
-  GL_NonExistant,
-  GL_Weak,
-  GL_Normal,
-  GL_Heavy,
-  GL_Oppressive,
-  GL_SpineCrushing
-};
-
-enum SizeCategory {
-  SC_Small,
-  SC_EarthLike,
-  SC_Large,
-  SC_Huge,
-  SC_Giant
-};
-
-typedef struct PlanetDescriptionData {
-  bool set;
-
-  double diameter;
-  SizeCategory sc;
-
-  double temperature;
-  TemperatureLevel tl;
-  AtmosphereLevel al;
-  WindLevel wl;
-
-  double density;
-  double gravitational_constant;
-  GravityLevel gl;
-
-  std::list<std::pair<Element_ptr, double>> apparent_composition;
-} PlanetDescriptionData;
+#include "description.hpp"
 
 class Planet {
 
