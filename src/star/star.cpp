2e8b0d2addc1887d70fa23692656c90cf6ed6fdf
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-19 20:47:23 +0100

    Started to flesh out star obj

---
+++ src/star/star.cpp
@@ -0,0 +1,11 @@
+#include "star.hpp"
+
+Star::Star(double position) : SystemObject(position)
+{
+  // do nothing
+}
+
+Star::~Star()
+{
+  // do nothing
+}
--- src/star/star.hpp
+++ src/star/star.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
-class Star {
-  // TODO
+#include "../types.hpp"
+#include "../system/object.hpp"
+
+class Star : public SystemObject {
+
+  private:
+
+    ElementalAbundance composition;
+    double density, diameter, temperature;
+
+  public:
+
+    Star(double position);
+    ~Star();
+
+    static Star_ptr generate();
 };
