456e16f6c09f2d1d128076fcd2ef68b4e872eadc
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-19 10:30:34 +0100

    Stripped back bootstrap event and do nothing; will fill in later

--- src/colony/bootstrap.cpp
+++ src/colony/bootstrap.cpp
@@ -4,18 +4,6 @@
 #include "bootstrap.hpp"
 
 
-BootstrapColonyEvent::BootstrapColonyEvent(Colony &colony) : colony(colony)
-{
-  this->exec_time = this->colony.get_inception_time() + 0.1;
-}
-
-
-BootstrapColonyEvent::~BootstrapColonyEvent()
-{
-  // 
-}
-
-
 BootstrapColonyEvent_ptr BootstrapColonyEvent::create(Colony &colony)
 {
   return std::make_shared<BootstrapColonyEvent>(colony);
@@ -31,13 +19,5 @@ Event_ptr Colony::get_bootstrap_event()
 void BootstrapColonyEvent::execute(Universe_ptr universe)
 {
   (void) universe;
-
-  std::cerr << "bootstrap!" << std::endl;
-  this->colony.add_message("bootstrap!");
-}
-
-
-double BootstrapColonyEvent::get_time()
-{
-  return this->exec_time;
+  // TODO
 }
--- src/colony/bootstrap.hpp
+++ src/colony/bootstrap.hpp
@@ -9,16 +9,19 @@ class BootstrapColonyEvent : public virtual Event{
   private:
 
     Colony &colony;
-    double exec_time;
 
   public:
 
-    BootstrapColonyEvent(Colony &colony);
-    ~BootstrapColonyEvent();
+    BootstrapColonyEvent(Colony &colony) : colony(colony) {}
+    ~BootstrapColonyEvent() {}
 
     static BootstrapColonyEvent_ptr create(Colony &colony);
 
     void execute(Universe_ptr universe) override;
-    double get_time() override;
+
+    double get_time() override
+    {
+      return -1.0;
+    }
 };
 
