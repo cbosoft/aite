10b19e7c1b482212501375d1baa85e6ecbeaf67c
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-21 19:35:25 +0100

    Added descriptive comment about history elements

--- src/history/history.hpp
+++ src/history/history.hpp
@@ -3,6 +3,23 @@
 
 #include "../types.hpp"
 
+/*
+ * A history element is a facet of the planet/star/system/galaxy past that a
+ * colony has uncovered, and thus gained some experience or knowledge that helps
+ * them develop.
+ *
+ * A history is generated for an object with history upon generation. This
+ * history will impact on the description of the object (an intense bombardment
+ * of meteors will result in craters for example), it will impact in the
+ * generation sub-objects (a galaxy with a black hole neighbour will have
+ * gravity wrenched systems that are sub-normal, an exploded star will shower
+ * rich deposits of minerals across the galaxy and so on).
+ *
+ * A history event is super-normal: normal expected events in the lifetime of
+ * the object go without saying, and are not allocated events.
+ *
+ * */
+
 enum HistoryElementTriggerType {HE_TriggerUnderground, HE_TriggerSurface, HE_TriggerOuterSpace};
 
 enum HistoryElementRewardType {HE_RewardTechnology, HE_RewardCulture};
