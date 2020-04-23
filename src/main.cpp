dbef786fa7333ee9d4ff0a14ad28726989571376
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-23 22:43:58 +0100

    Testing the history element gen and load

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,18 @@
 #include "universe/universe.hpp"
 #include "util/thesaurus.hpp"
 #include "util/random.hpp"
+#include "history/history.hpp"
 
 int main(void)
 {
 
   seed(time(NULL));
 
-  Thesaurus &thesaurus = Thesaurus::getref();
+  // Thesaurus &thesaurus = Thesaurus::getref();
 
-  std::cerr << thesaurus.get_random_synonym("home", "noun") << std::endl;
+  // std::cerr << thesaurus.get_random_synonym("home", "noun") << std::endl;
+
+  auto history = HistoryElement::generate(HO_Any);
+  std::cerr << history->describe() << std::endl;
 
 }
