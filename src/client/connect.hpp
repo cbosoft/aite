7fd1a4cf40beebcd9a2e2502c3979b523700a73f
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-24 17:55:47 +0100

    Added client method to sync data from server

--- src/client/connect.hpp
+++ src/client/connect.hpp
@@ -1,16 +1,27 @@
 #pragma once
 #include <string>
+#include <list>
+
 #include "reply.hpp"
+
+
+typedef struct ColonyState {
+  std::list<std::string> messages;
+  std::list<std::string> activities;
+} ColonyState;
 
 
 class ServerConnection {
 
   private:
+
     int fd;
+    ColonyState state;
 
   public:
     ServerConnection(const char *address, int port);
     ~ServerConnection();
 
     ServerReply send(std::string message);
+    void sync();
 };
