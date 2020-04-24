cd7194abdb877ea56a6745f691bc15ecbebd34be
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-24 23:05:55 +0100

    Seed random on start

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,8 +1,14 @@
+#include <ctime>
+#
 #include "server.hpp"
 #include "args.hpp"
+#include "../util/random.hpp"
 
 int main(int argc, const char **argv)
 {
+
+  seed(time(NULL));
+
   ServerArgs args = parse_args(argc, argv);
 
   GameServer game = GameServer(args.port);
