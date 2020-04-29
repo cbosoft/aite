47ed118d526936543bee647a1963b4158b6ae852
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-04-29 20:04:59 +0100

    Slightly nicer exponential formatting

--- src/util/format.cpp
+++ src/util/format.cpp
@@ -3,17 +3,24 @@
 
 #include "format.hpp"
 
+std::string format_number_as_scientific(double number)
+{
+  std::stringstream ss;
+
+  double p = std::round(std::log10(number));
+  double v = number / std::pow(10.0, p);
+
+  ss << v << "×10^" << int(p);
+  return ss.str();
+}
 
 std::string format_number(double number)
 {
   std::stringstream ss;
 
-  if (number > 1000)
-    ss << std::scientific;
-
-  if (std::log10(number) < -1)
-    ss << std::scientific;
-
-  ss << number;
+  if ((number > 1000) or (std::log10(number) < -1.0))
+    ss << format_number_as_scientific(number);
+  else
+    ss << number;
   return ss.str();
 }
