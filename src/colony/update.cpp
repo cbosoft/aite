75b97e951a5d9a29d0a6da497a8d17620a797032
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-03 21:11:18 +0100

    Added research progress into update

--- src/colony/update.cpp
+++ src/colony/update.cpp
@@ -16,55 +16,59 @@ void Colony::update(double dt)
   //       << BOLD << "First colonial birth." << RESET);
   //   notified_1 = true;
   // }
-  
+ 
+
+  // Research progress 
   this->stats.population.number
     .multiply_base(1.0 + this->stats.derived.growth_rate->get_value()*dt);
 
-  // if (this->research_effort == 0.0)
-  //   return;
+  if (this->stats.technology.research_effort == 0.0) {
 
-  // double research_each = this->research_effort;
-  // switch (this->research_focus) {
+   double research_each = this->stats.technology.research_effort;
+   switch (this->research_focus) {
 
-  //   case RF_Equal:
-  //     research_each /= 5.0;
-  //     this->technology_stats.agriculture.increase_base(research_each);
-  //     this->technology_stats.materials_gathering.increase_base(research_each);
-  //     this->technology_stats.materials_processing.increase_base(research_each);
-  //     this->technology_stats.power_generation.increase_base(research_each);
-  //     this->technology_stats.astrogation.increase_base(research_each);
-  //     break;
+     case RF_Equal:
+       research_each /= 5.0;
+       this->stats.technology.agriculture.increase_base(research_each);
+       this->stats.technology.materials_gathering.increase_base(research_each);
+       this->stats.technology.materials_processing.increase_base(research_each);
+       this->stats.technology.power_generation.increase_base(research_each);
+       this->stats.technology.astrogation.increase_base(research_each);
+       break;
 
-  //   case RF_Agriculture:
-  //   case RF_Food:
-  //     this->technology_stats.agriculture.increase_base(research_each);
-  //     break;
+     case RF_Agriculture:
+     case RF_Food:
+       this->stats.technology.agriculture.increase_base(research_each);
+       break;
 
-  //   case RF_Resource:
-  //     research_each /= 3.0;
-  //     this->technology_stats.materials_gathering.increase_base(research_each);
-  //     this->technology_stats.materials_processing.increase_base(research_each);
-  //     this->technology_stats.power_generation.increase_base(research_each);
-  //     break;
+     case RF_Resource:
+       research_each /= 3.0;
+       this->stats.technology.materials_gathering.increase_base(research_each);
+       this->stats.technology.materials_processing.increase_base(research_each);
+       this->stats.technology.power_generation.increase_base(research_each);
+       break;
 
-  //   case RF_Astrogation:
-  //   case RF_Exploration:
-  //     this->technology_stats.astrogation.increase_base(research_each);
-  //     break;
+     case RF_Astrogation:
+     case RF_Exploration:
+       this->stats.technology.astrogation.increase_base(research_each);
+       break;
 
-  //   case RF_MaterialsGathering:
-  //     this->technology_stats.materials_gathering.increase_base(research_each);
-  //     break;
+     case RF_MaterialsGathering:
+       this->stats.technology.materials_gathering.increase_base(research_each);
+       break;
 
-  //   case RF_MaterialsProcessing:
-  //     this->technology_stats.materials_processing.increase_base(research_each);
-  //     break;
+     case RF_MaterialsProcessing:
+       this->stats.technology.materials_processing.increase_base(research_each);
+       break;
 
-  //   case RF_Power:
-  //     this->technology_stats.power_generation.increase_base(research_each);
-  //     break;
-  // }
+     case RF_Power:
+       this->stats.technology.power_generation.increase_base(research_each);
+       break;
+   }
+  }
 
+
+  // Update projects
   for (auto it = this->projects.begin(); it != this->projects.end(); it++) {
     auto project = it->second;
     auto status = project->update();
