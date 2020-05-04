7782d117e9613418ac9327a6706f74ffc9f72c01
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-04 20:51:57 +0100

    Load/save last used colony name and server details from/to disk

--- src/client/args.cpp
+++ src/client/args.cpp
@@ -1,4 +1,6 @@
-#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 #include "../util/args.hpp"
 #include "../util/exception.hpp"
@@ -9,13 +11,62 @@
 ClientArgs default_args()
 {
   ClientArgs args = {
+    .try_load_config_from_file = true,
     .server_address = "127.0.0.1",
     .server_port = DEFAULT_PORT,
     .colony_name = "guest",
-    .mode = CM_GUI
+    .mode = CM_TUI,
+    .command = "status",
+    .args = {}
   };
 
   return args;
+}
+
+void parse_args_from_file(ClientArgs &args)
+{
+  const char *hdir = std::getenv("HOME");
+  std::string path = Formatter() << hdir << "/.aite_config";
+  std::ifstream ifs(path);
+
+  if (ifs.fail())
+    return;
+
+  std::string line, key, value;
+  while (std::getline(ifs, line)) {
+    std::stringstream ss(line);
+    std::getline(ss, key, ' ');
+    std::getline(ss, value);
+
+    if (key.compare("colony_name") == 0) {
+      args.colony_name = strdup(value.c_str());
+    }
+    else if (key.compare("server_port") == 0) {
+      args.server_port = std::stoi(value);
+    }
+    else if (key.compare("server_address") == 0) {
+      args.server_address = strdup(value.c_str());
+    }
+
+  }
+
+  (void) args;
+
+}
+
+void save_args_to_file(ClientArgs &args)
+{
+  const char *hdir = std::getenv("HOME");
+  std::string path = Formatter() << hdir << "/.aite_config";
+  std::ofstream ofs(path);
+  if (ofs.fail())
+    return;
+
+  ofs
+    << "colony_name " << args.colony_name << std::endl
+    << "server_address " << args.server_address << std::endl
+    << "server_port " << args.server_port << std::endl
+    ;
 }
 
 
@@ -26,12 +77,15 @@ ClientArgs parse_args(int argc, const char **argv)
   argc--; argv++;
   for (int i = 0; i < argc; i++) {
     if (ARG_EITHER("-i", "--server-ip-address")) {
+      args.try_load_config_from_file = false;
       args.server_address = argv[++i];
     }
     else if (ARG_EITHER("-p", "--server-port")) {
+      args.try_load_config_from_file = false;
       args.server_port = atoi(argv[++i]);
     }
     else if (ARG_EITHER("-c", "--colony-name")) {
+      args.try_load_config_from_file = false;
       args.colony_name = argv[++i];
     }
     else if (ARG_EITHER("-m", "--mode")) {
@@ -53,6 +107,10 @@ ClientArgs parse_args(int argc, const char **argv)
       throw ArgumentError(Formatter() << "Argument " << argv[i] << " not recognised.");
     }
   }
+
+  if (args.try_load_config_from_file)
+    parse_args_from_file(args);
+  save_args_to_file(args);
 
   return args;
 }
--- src/client/args.hpp
+++ src/client/args.hpp
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <string>
+#include <list>
+
 enum ClientMode { CM_Batch, CM_TUI, CM_GUI };
 
 typedef struct ClientArgs {
+  bool try_load_config_from_file;
   const char *server_address;
   int server_port;
   const char *colony_name;
   ClientMode mode;
+  std::string command;
+  std::list<std::string> args;
 } ClientArgs;
 
 ClientArgs parse_args(int argc, const char **argv);
