bd2e4b6afc28cf232d329df4bb9d230dd39012dc
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-10 17:02:54 +0100

    Fixed system object positioning

--- src/system/generate.cpp
+++ src/system/generate.cpp
@@ -14,11 +14,11 @@ System_ptr System::generate(Vec3 position, Galaxy_ptr galaxy)
   double source_luminosity = star->get_luminosity();
 
   int nplanets = std::round(normal_rand(10, 2));
-  double object_distance = 0.0;
+  double object_distance = star->get_radius() + uniform_rand(0.1, 0.3);
   for (int i = 0; i < nplanets; i++) {
-    object_distance += lognormal_rand(10, 1);
     SystemObject_ptr object = SystemObject::generate(system, object_distance, source_luminosity, SO_Planet);
     system->objects.push_back(object);
+    object_distance += uniform_rand(0.05, 0.8) + object->get_diameter();
   }
 
   return system;
@@ -35,23 +35,23 @@ System_ptr System::generate_inhabitable(Vec3 position, Galaxy_ptr galaxy, System
   double source_luminosity = star->get_luminosity();
 
   int nplanets = std::round(normal_rand(10, 2));
-  double object_distance = 0.0;
+  double object_distance = star->get_radius() + uniform_rand(0.1, 0.3);
   int i = 0;
   for (; i < nplanets/2; i++) {
-    object_distance += lognormal_rand(10, 1);
     SystemObject_ptr object = SystemObject::generate(system, object_distance, source_luminosity, SO_Planet);
     system->objects.push_back(object);
+    object_distance += uniform_rand(0.05, 0.8) + object->get_diameter();
   }
 
   i++;
-  object_distance += lognormal_rand(10, 1);
   object = SystemObject::generate(system, object_distance, source_luminosity, SO_EarthlikePlanet);
   system->objects.push_back(object);
+  object_distance += uniform_rand(0.05, 0.8) + object->get_diameter();
 
   for (; i < nplanets/2; i++) {
-    object_distance += lognormal_rand(10, 1);
     SystemObject_ptr object = SystemObject::generate(system, object_distance, source_luminosity, SO_Planet);
     system->objects.push_back(object);
+    object_distance += uniform_rand(0.05, 0.8) + object->get_diameter();
   }
 
   return system;
