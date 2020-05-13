663265d17f16c092eb986fc4b9c893e3acf19233
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-05-13 15:07:06 +0100

    Moved chemical component distribution generator to object generator

--- src/object/earthlike_planet.cpp
+++ src/object/earthlike_planet.cpp
@@ -9,6 +9,63 @@
 #include "planet.hpp"
 
 
+static ChemicalComposition get_distribution(double density)
+{
+
+  std::array<double, 118> abundance;
+  double nrho = (std::log(density) + 3.0)/9.0;
+  int high_abundant_range_start = int(nrho*7.0/0.33) + uniform_rand_i(0, 3);
+  int high_abundant_range_end = uniform_rand_i(5, 8) + int(13.0*nrho/0.33) + high_abundant_range_start;
+
+
+  for (int z = 1; z <= 118; z++) {
+    double a = 118.0 / double(z);
+
+    if (z % 6) {
+      a *= 4.0;
+    }
+    else if (z % 2) {
+      a *= 2.0;
+    }
+
+    double mult = 1.0;
+    if (z < high_abundant_range_start) {
+      if (z < 3) {
+        mult = 0.01/density;
+      }
+      else {
+        mult = 0.1;
+      }
+    }
+    else if (z >= 95) {
+      mult = 1e-5*density;
+    }
+    else if (z > high_abundant_range_end) {
+      mult = 1e-2;
+    }
+
+    a = a*mult;
+    abundance[z-1] = a*uniform_rand(0.1, 10.0);
+
+  }
+
+  ChemicalComposition dist;
+
+  double total = 0.0;
+  for (int i = 0; i < 118; i++)
+    total += abundance[i];
+
+  for (int i = 0; i < 118; i++) {
+    if (abundance[i] <= 0) continue;
+    auto elem = Element::get_element_by_n(i+1);
+    auto pair = std::make_pair(elem, double(abundance[i]) / total);
+    dist.push_back(pair);
+  }
+
+  return dist;
+}
+
+
 SystemObject_ptr EarthlikePlanet::generate(System_ptr system, double position, double luminosity)
 {
   auto planet = std::make_shared<EarthlikePlanet>(system, position);
@@ -25,7 +82,7 @@ SystemObject_ptr EarthlikePlanet::generate(System_ptr system, double position, d
   planet->temperature = normal_rand(310, 10.0);
 
   // elements
-  planet->composition = Element::get_distribution(planet->density);
+  planet->composition = get_distribution(planet->density);
   planet->composition.sort(CompositionSortByAbundanceDescending());
 
   double planet_surface_area = planet->object_diameter*planet->object_diameter*CONST_AUm*CONST_AUm*M_PI;
@@ -46,4 +103,3 @@ SystemObject_ptr EarthlikePlanet::generate(System_ptr system, double position, d
 
   return planet;
 }
-
