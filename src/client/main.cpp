fd337874630e8b815f03ea41160ae8f94de98959
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-07-04 11:52:22 +0100

    Remove alternate control methods (leaving cli only)

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -7,47 +7,13 @@
 #include "client.hpp"
 
 
-void batchmain(ClientArgs args)
-{
-  GameClient conn = GameClient(args.server_address, args.server_port, args.colony_name);
-  conn.execute(args.command, args.args);
-}
-
-
-void tuimain(ClientArgs args)
-{
-  (void)args;
-  std::cerr << "TUI not yet available." << std::endl;
-}
-
-
-void guimain(ClientArgs args)
-{
-  (void)args;
-  std::cerr << "GUI not yet available." << std::endl;
-}
-
-
 int main(int argc, const char **argv)
 {
   ClientArgs args = parse_args(argc, argv);
 
   try {
-  switch (args.mode) {
-
-    case CM_Batch:
-      batchmain(args);
-      break;
-
-    case CM_TUI:
-      tuimain(args);
-      break;
-
-    case CM_GUI:
-      guimain(args);
-      break;
-
-  }
+    GameClient conn = GameClient(args.server_address, args.server_port, args.colony_name);
+    conn.execute(args.command, args.args);
   }
   catch (const SocketError &e) {
     std::cerr
